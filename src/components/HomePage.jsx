1e19cd50b47110153e804b4ad9806504bcbe40ba
Shafee <shafeelinks@gmail.com>
2021-01-11 19:45:00 -0500

    code along

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,20 @@
+const HomePage = (props) => {
+    return (
+        <div>
+            <h1>My Hardware Store</h1>
+            <span>Currently On Sale: {props.saleItem}</span>
+            <span><button onClick={props.toggleEditSaleItem}>
+                {props.editable ? "Hide" : "Edit Sale Item"}
+                </button></span>
+            <div>
+               {props.editable ? <div><input 
+                onChange={props.handleItemCurrentlyOnSaleChange}
+                type="text"
+                value={props.itemCurrentlyOnSale}
+               /></div> : null}
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
